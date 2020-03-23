342d31a8e717827e499d8a2d9682c0c693a86267
MucaBordini <mucabordini@gmail.com>
2020-03-23 11:55:59 -0300

    Adicionado os paises no select e tabela é limpa sempre que há uma nova busca

--- javascript/searchResult.js
+++ javascript/searchResult.js
@@ -1,6 +1,38 @@
 // Adiciona evento de click no botão de Busca
 document.querySelector('.botaobusca').addEventListener('click', listaGET);
 
+// Adiciona evento quando carrega a página
+window.addEventListener('load', countriesGET());
+
+// Realiza a requisição GET para preencher o 'select'
+function countriesGET() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.covid19api.com/countries', true);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      // Chama funcão para construir tabela
+      buildSelect(JSON.parse(request.responseText));
+    }
+  };
+  request.send();
+}
+
+function buildSelect(paises){
+  var selectPais = document.querySelector('.countrybusca');
+  selectPais.length = 0;
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Escolha um país';
+  selectPais.add(defaultOption);
+  selectPais.selectedIndex = 0;
+  let option;
+    for (let i = 0; i < paises.length; i++) {
+      option = document.createElement('option');
+      option.text = paises[i].Country;
+      option.value = paises[i].Slug;
+      selectPais.add(option);
+    }
+}
+
 // Realiza a requisicão do tipo GET
 function listaGET() {
   let country = document.querySelector('.countrybusca').value;
@@ -19,6 +51,10 @@ function listaGET() {
 
 // Constroi a tabela
 function buildTable(lista) {
+  var table = document.querySelector('tbody');
+  while (table.rows.length > 0) { 
+    table.deleteRow(0);
+  }
   lista.forEach(element => {
   // Inicializa variáveis
   var new_tbody = document.querySelector('tbody');
